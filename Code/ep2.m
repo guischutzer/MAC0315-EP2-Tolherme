44e3ce484c9244a5df24aa5de548fb214ddbbd33
Guilherme Schützer <guischutzer@gmail.com>
2015-05-14 21:44:00 -0300

    Umas coisa

--- Code/ep2.m
+++ Code/ep2.m
@@ -7,24 +7,33 @@ b = [8 12 4 6]
 c = [1 2 3 4]
 m = 4
 n = 7
-x = [0 0 0 8 1 2 4 6]
+x = [0 0 0 8 12 4 6]
 
 function simplex (A, b, c, m, n, x)
 
   # criação do vetor de índices
-  xB = []
+  bind = []
   B = A
   for i = 1:n
     if x(i) != 0
-      xB(end + 1) = true
+      bind(end + 1) = i
     else 
-      xB(end + 1) = false
       B(:, 1) = []
    endif
-   
-   
-   
   endfor
+  
+  [L, U] = lu(B)
+  
+  # criação de um vetor com os custos
+  v = []
+  for j = 1:n
+    if bind != j
+      y = transpose(A(:, i)) / L
+      u = y / U
+      v(end + 1) = c(j) - c*transpose(u)
+    endif
+  endfor
+  
   
   
 endfunction
