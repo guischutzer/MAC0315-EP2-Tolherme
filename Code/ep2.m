c08031063ce3ed522cd0e4fe0f8a96a8165853d5
Guilherme Schützer <guischutzer@gmail.com>
2015-05-15 00:33:30 -0300

    didshfshudf

--- Code/ep2.m
+++ Code/ep2.m
@@ -4,38 +4,50 @@ A = [1 1 2 1 0 0 0;
      1 0 0 0 0 1 0;
      0 1 0 0 0 0 1]
 b = [8 12 4 6]
-c = [1 2 3 4]
+c = [10 20 30 40 50 60 70]
 m = 4
 n = 7
 x = [0 0 0 8 12 4 6]
 
-function simplex (A, b, c, m, n, x)
+function retval = simplex (A, b, c, m, n, x)
 
   # criação do vetor de índices
   bind = []
   B = A
-  for i = 1:n
+  for i = fliplr(1:n)
     if x(i) != 0
       bind(end + 1) = i
+      cB(end + 1) = c(i)
     else 
-      B(:, 1) = []
+      B(:, i) = []
    endif
   endfor
+  bind = fliplr(bind)
+  cB   = fliplr(cB)
   
   [L, U] = lu(B)
   
-  # criação de um vetor com os custos
-  v = []
+  # criação de um vetor com os custos cj-barra
+  # associados às variáveis não-básicas
+  cjbarra = []
   for j = 1:n
     if bind != j
-      y = transpose(A(:, i)) / L
-      u = y / U
-      v(end + 1) = c(j) - c*transpose(u)
+      # encontrar p' t. q. c'B * B^(-1) = p'
+      y = cB / transpose(U)
+      p = y / transpose(L)
+      cjbarra(end + 1) = c(j) - p * A(:, j)
+    else 
+      cjbarra(end + 1) = 0
     endif
   endfor
   
+  if cjbarra >= 0
+    return
+  endif
   
+  
+  retval = 1
   
 endfunction
 
-[ind v] = simplex(A, b, c, m, n, x)
+[v] = simplex(A, b, c, m, n, x)
